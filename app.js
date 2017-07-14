0d5e305f05bc3a172a78daf3cddea4a5ed64f4f1
Ryan Bryce <ryan.bryce1992@gmail.com>
2017-07-14 15:20:22 -0600

    added event listener for hold button. when clicked it will add the players score for that round to the player's overall score stored in the players array. it will check to see if the players overall score is >= 100 and if so it will change classes and textContent to indicate that they are the winner for that round. if the players overall score is not over 100 i call the next function located on the global scope to switch the active player, also it helps to keep it DRY.

--- app.js
+++ app.js
@@ -10,22 +10,64 @@ GAME RULES:
 */
 
 var score, roundScore, activePlayer;
-score = [0,0];
+scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
 //console.log(dice);
 
 document.querySelector('.dice').style.display = 'none';
-document.getElementById('score-0').textContent = '0'
-document.getElementById('score-1').textContent = '0'
-document.getElementById('current-0').textContent = '0'
-document.getElementById('current-1').textContent = '0'
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
 document.querySelector('.btn-roll').addEventListener('click', () => {
+  //1. Random Number
   var dice = Math.floor(Math.random() * 6) + 1;
+
+  //2. Display the result
   var diceDom = document.querySelector('.dice');
-  diceDom.style.display = 'block'
-  diceDom.src = 'dice-' + dice + '.png'
-  var currentScore = document.querySelector('#current-' + activePlayer).textContent;
-})
+  diceDom.style.display = 'block';
+  diceDom.src = 'dice-' + dice + '.png';
+
+  //3. Update the round score IF the rolled number was not a 1
+  if (dice !== 1) {
+    //Add score
+    roundScore += dice;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  }else{
+    //Next pLayer
+    nextPlayer();
+  }
+});
+
+document.querySelector('.btn-hold').addEventListener('click', () => {
+  //1. add CURRENT to GLOBAL score;
+  scores[activePlayer] += roundScore;
+  //2. update the UI
+  document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+  //3. check if player won the game
+  if (scores[activePlayer] >= 20) {
+    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('#name-' + activePlayer).textContent = 'WINNER'
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+    document.querySelector('.player-'+ activePlayer + '-panel').classList.remove('active');
+  }else {
+    nextPlayer();
+  }
+});
+
+
+
+function nextPlayer() {
+  //Next pLayer
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  roundScore = 0;
+  document.getElementById('current-0').textContent = 0;
+  document.getElementById('current-1').textContent = 0;
+
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+  document.querySelector('.dice').style.display = 'none';
+}
