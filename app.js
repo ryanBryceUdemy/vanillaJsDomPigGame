2a268056044d2264b902f55e14204cca10f64a87
Ryan Bryce <ryan.bryce1992@gmail.com>
2017-07-14 12:55:10 -0600

    added DOM manipulations to set starting scores to 0 on load, and added eevent listeners for dice roll button to dynamically change the dice image

--- app.js
+++ app.js
@@ -8,3 +8,24 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var score, roundScore, activePlayer;
+score = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+//console.log(dice);
+
+document.querySelector('.dice').style.display = 'none';
+document.getElementById('score-0').textContent = '0'
+document.getElementById('score-1').textContent = '0'
+document.getElementById('current-0').textContent = '0'
+document.getElementById('current-1').textContent = '0'
+
+document.querySelector('.btn-roll').addEventListener('click', () => {
+  var dice = Math.floor(Math.random() * 6) + 1;
+  var diceDom = document.querySelector('.dice');
+  diceDom.style.display = 'block'
+  diceDom.src = 'dice-' + dice + '.png'
+  var currentScore = document.querySelector('#current-' + activePlayer).textContent;
+})
